f2f0f6b2808f84a9d7b4f58ba514b2601a7ddf00
Konstantin Fedorov <cos9191@mail.ru>
2022-08-24 20:36:29 +0700

    добавил svg-ки в спрайт, паре img в index добавил webP версии, расставил по PP index, оживил галерею, лайки реагируют на нажатие

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,14 @@
 let headerList = document.querySelector('.header__list');
 let headerToggle = document.querySelector('.header__toggle-button-wrapper');
 let headerToggleButton = document.querySelector('.header__toggle-button');
+let contentLikeWrapper = document.querySelectorAll('.content__like-wrapper');
+let contentLikeIcon = document.querySelectorAll('.content__like-icon');
 
+// Показываем меню и скрываем бюргер, если JS не загрузился
 headerList.classList.remove('header__list--nojs');
 headerToggleButton.classList.remove('header__toggle-button--nojs');
 
+// Открываем/закрываем меню навигации по нажатию на бургер, анимируем бургер
 headerToggle.addEventListener('click', function () {
   if (headerList.classList.contains('header__list--closed')) {
     headerList.classList.remove('header__list--closed');
@@ -18,3 +22,16 @@ headerToggle.addEventListener('click', function () {
     headerToggleButton.classList.remove('header__toggle-button--opened');
   }
 });
+
+// Красим лайк по нажатию
+for (let i of contentLikeIcon) {
+  i.addEventListener('click', function () {
+    if (i.classList.contains('content__like-icon--activated')) {
+      i.classList.remove('content__like-icon--activated');
+    } else {
+      i.classList.add('content__like-icon--activated');
+    }
+  });
+};
+
+
