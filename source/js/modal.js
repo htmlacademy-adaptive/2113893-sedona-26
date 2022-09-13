027393d3fd4f951172fe352845377cb5a1997259
Konstantin Fedorov <cos9191@mail.ru>
2022-09-13 15:49:26 +0700

    добавил анимации кнопкам и хэштэгу, при отправке формы открывается модальное окно, его можно закрыть

---
+++ source/js/modal.js
@@ -0,0 +1,12 @@
+let form = document.querySelector(".form__grid");
+let modalSuccess = document.querySelector(".modal-container");
+let modalButton = document.querySelector(".modal__button");
+
+form.onsubmit = function (evt) {
+  evt.preventDefault();
+  modalSuccess.classList.remove("visually-hidden");
+}
+
+modalButton.onclick = function() {
+  modalSuccess.classList.add("visually-hidden");
+}
